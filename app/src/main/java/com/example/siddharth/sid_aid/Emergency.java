b8368cb39e277abfebbe702d55de2a878729fb34
siddinho <sidvohra95@live.com>
2017-02-18 09:49:09 +0530

    Current Location on Map Added.

---
+++ app/src/main/java/com/example/siddharth/sid_aid/Emergency.java
@@ -0,0 +1,168 @@
+package com.example.siddharth.sid_aid;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Emergency extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+    LocationManager locationManager;
+    double currentLat;
+    double currentLong;
+    TextView locationtxt;
+    float zoomLevel = (float) 16.0;
+    private GoogleMap mMap;
+    //Location location;
+
+    //Zoom + Curent Location on map added
+    //Check for auto-updation.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // my code
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        locationtxt = (TextView) findViewById(R.id.locationtxt);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device.
+         * This method will only be triggered once the user has installed
+         Google Play services and returned to the app.
+         */
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        onMapReady1(mMap);
+
+
+       /* if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+
+        // Add a marker in Sydney and move the camera
+        LatLng currentcoordinates = new LatLng(currentLat,currentLong);
+        mMap.addMarker(new
+                MarkerOptions().position(currentcoordinates).title("You Are Here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentcoordinates));
+*/
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLat = location.getLatitude();
+        currentLong = location.getLongitude();
+        locationtxt.setText("Your Co-rdinates Are\n" + currentLat + "   " + currentLong);
+        onMapReady1(mMap);// for recalling purpose
+        //onMapReady(googlemap);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public void onMapReady1(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+
+        // Add a marker in Sydney and move the camera
+        LatLng currentcoordinates = new LatLng(currentLat, currentLong);
+
+        //This goes up to 21
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel))
+
+        mMap.addMarker(new
+                MarkerOptions().position(currentcoordinates).title("You Are Here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentcoordinates, zoomLevel));
+
+    }
+
+}
--- app/src/main/java/com/example/siddharth/sid_aid/MainActivity.java
+++ app/src/main/java/com/example/siddharth/sid_aid/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.siddharth.sid_aid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ViewFlipper viewFlipper= (ViewFlipper) findViewById(R.id.viewflipper1);
         viewFlipper.setAutoStart(true);
-        viewFlipper.setFlipInterval(400);
+
+
+        //Second Day Code
+
+
+        final Button Emergency = (Button) findViewById(R.id.emergencybtn);
+        Emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Emergency.class);
+                startActivity(i);
+            }
+        });
     }
 }
